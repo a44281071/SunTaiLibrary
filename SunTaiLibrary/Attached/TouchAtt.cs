4e33981924cd25a6bb5dd11c1ac0edd34c2ede0b
SunTai（三台） <a44281071@sina.com>
2018-07-24 17:04:43 +0800

    add attached.

---
+++ SunTaiLibrary/Attached/TouchAtt.cs
@@ -0,0 +1,52 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+
+namespace SunTaiLibrary.Attached
+{
+  public static class TouchAtt
+  {
+    public static bool GetIsEnabled(DependencyObject obj)
+    {
+      return (bool)obj.GetValue(IsEnabledProperty);
+    }
+
+    public static void SetIsEnabled(DependencyObject obj, bool value)
+    {
+      obj.SetValue(IsEnabledProperty, value);
+    }
+
+    /// <summary>
+    /// 启用触摸滚动
+    /// </summary>
+    public static readonly DependencyProperty IsEnabledProperty =
+           DependencyProperty.RegisterAttached("IsEnabled"
+             , typeof(bool)
+             , typeof(TouchAtt)
+             , new PropertyMetadata(false, OnEnabledChanged));
+
+    private static void OnEnabledChanged(DependencyObject d, DependencyPropertyChangedEventArgs e)
+    {
+      if (d is UIElement ele)
+      {
+        if (e.NewValue != e.OldValue)
+        {
+          if ((bool)e.NewValue)
+          {
+            ScrollViewer.SetPanningMode(ele, PanningMode.Both);
+            ele.ManipulationBoundaryFeedback += Ele_ManipulationBoundaryFeedback;
+          }
+          else
+          {
+            ScrollViewer.SetPanningMode(ele, PanningMode.None);
+            ele.ManipulationBoundaryFeedback -= Ele_ManipulationBoundaryFeedback;
+          }
+        }
+      }
+    }
+
+    private static void Ele_ManipulationBoundaryFeedback(object sender, System.Windows.Input.ManipulationBoundaryFeedbackEventArgs e)
+    {
+      e.Handled = true;
+    }
+  }
+}
---
+++ SunTaiLibrary/Converters/RowIndexConverter.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+
+namespace SunTaiLibrary.Converters
+{
+  public class RowIndexConverter : IValueConverter
+  {
+    public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+    {
+      var ele = (FrameworkElement)value;
+      var parent = parameter as ItemsControl ?? ele.GetVisualTreeParent<ItemsControl>();
+      int index = parent.ItemContainerGenerator.IndexFromContainer(parent.ItemContainerGenerator.ContainerFromItem(ele.DataContext));
+      return index + 1;
+    }
+
+    public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+    {
+      throw new NotImplementedException();
+    }
+  }
+
+}

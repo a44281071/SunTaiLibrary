7bbfd27a3c7b2bdda9426eec181fb12e58b5fc77
SamTi <a44281071@live.com>
2019-07-26 15:33:21 +0800

    add class.

---
+++ SunTaiLibrary/EventArgs.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SunTaiLibrary
+{
+  public class EventArgs<T> : EventArgs
+  {
+    public EventArgs()
+    {
+    }
+
+    public EventArgs(T args)
+    {
+      Args = args;
+    }
+
+    public T Args { get; }
+  }
+}

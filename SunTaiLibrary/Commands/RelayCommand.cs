a635daf0fd5dcc6e4704374e465d2e00ff47292e
SamTi <a44281071@sina.com>
2020-08-07 15:25:18 +0800

    add helper class for UIElement.

--- SunTaiLibrary/Commands/RelayCommand.cs
+++ SunTaiLibrary/Commands/RelayCommand.cs
@@ -9,46 +9,62 @@ using System.Windows.Input;
 namespace SunTaiLibrary.Commands
 {
   /// <summary>
-  /// Simple relay command for internal use only.
+  /// Simple relay command.
   /// </summary>
   public class RelayCommand : ICommand
   {
-    #region Fields
-
-    private readonly Action _Execute;
-    private readonly Func<bool> _CanExecute;
-
-    #endregion Fields
-
-    #region Constructors
-
+    /// <summary>
+    /// Initializes a new instance of <see cref="RelayCommand"/>.
+    /// </summary>
+    /// <param name="execute">Delegate to execute when Execute is called on the command. </param>
+    /// <remarks><seealso cref="CanExecute"/> will always return true.</remarks>
     public RelayCommand(Action execute)
         : this(execute, null)
     {
     }
 
+    /// <summary>
+    /// Initializes a new instance of <see cref="RelayCommand"/>.
+    /// </summary>
+    /// <param name="execute">The execution logic.</param>
+    /// <param name="canExecute">The execution status logic.</param>
     public RelayCommand(Action execute, Func<bool> canExecute)
     {
       _Execute = execute ?? throw new ArgumentNullException(nameof(execute));
       _CanExecute = canExecute;
     }
 
-    #endregion Constructors
+    private readonly Action _Execute;
+    private readonly Func<bool> _CanExecute;
 
     #region ICommand Members
 
+    ///<summary>
+    ///Defines the method that determines whether the command can execute in its current state.
+    ///</summary>
+    ///<param name="parameter">Data used by the command.  If the command does not require data to be passed, this object can be set to null.</param>
+    ///<returns>
+    ///true if this command can be executed; otherwise, false.
+    ///</returns>
     [DebuggerStepThrough]
     public bool CanExecute(object parameter)
     {
-      return _CanExecute == null ? true : _CanExecute();
+      return _CanExecute == null || _CanExecute();
     }
 
+    ///<summary>
+    ///Occurs when changes occur that affect whether or not the command should execute.
+    ///</summary>
     public event EventHandler CanExecuteChanged
     {
       add { CommandManager.RequerySuggested += value; }
       remove { CommandManager.RequerySuggested -= value; }
     }
 
+    ///<summary>
+    ///Defines the method to be called when the command is invoked.
+    ///</summary>
+    ///<param name="parameter">Data used by the command. If the command does not require data to be passed, this object can be set to <see langword="null" />.</param>
     public void Execute(object parameter)
     {
       _Execute();
@@ -58,46 +74,64 @@ namespace SunTaiLibrary.Commands
   }
 
   /// <summary>
-  /// Simple relay command for internal use only.
+  /// Simple relay command.
   /// </summary>
   /// <typeparam name="T"></typeparam>
   internal class RelayCommand<T> : ICommand where T : class
   {
-    #region Fields
-
-    private readonly Action<T> _Execute;
-    private readonly Predicate<T> _CanExecute;
-
-    #endregion Fields
-
-    #region Constructors
+    /// <summary>
+    /// Initializes a new instance of <see cref="RelayCommand{T}"/>.
+    /// </summary>
+    /// <param name="execute">Delegate to execute when Execute is called on the command. </param>
+    /// <remarks><seealso cref="CanExecute"/> will always return true.</remarks>
 
     public RelayCommand(Action<T> execute)
         : this(execute, null)
     {
     }
 
+    /// <summary>
+    /// Initializes a new instance of <see cref="RelayCommand{T}"/>.
+    /// </summary>
+    /// <param name="execute">The execution logic.</param>
+    /// <param name="canExecute">The execution status logic.</param>
     public RelayCommand(Action<T> execute, Predicate<T> canExecute)
     {
       _Execute = execute ?? throw new ArgumentNullException(nameof(execute));
       _CanExecute = canExecute;
     }
 
-    #endregion Constructors
+    private readonly Action<T> _Execute;
+    private readonly Predicate<T> _CanExecute;
 
     #region ICommand Members
 
+    ///<summary>
+    ///Defines the method that determines whether the command can execute in its current state.
+    ///</summary>
+    ///<param name="parameter">Data used by the command.  If the command does not require data to be passed, this object can be set to null.</param>
+    ///<returns>
+    ///true if this command can be executed; otherwise, false.
+    ///</returns>
     [DebuggerStepThrough]
     public bool CanExecute(object parameter)
     {
-      return _CanExecute == null ? true : _CanExecute(parameter as T);
+      return _CanExecute == null || _CanExecute(parameter as T);
     }
 
+    ///<summary>
+    ///Occurs when changes occur that affect whether or not the command should execute.
+    ///</summary>
     public event EventHandler CanExecuteChanged
     {
       add { CommandManager.RequerySuggested += value; }
       remove { CommandManager.RequerySuggested -= value; }
     }
+
+    ///<summary>
+    ///Defines the method to be called when the command is invoked.
+    ///</summary>
+    ///<param name="parameter">Data used by the command. If the command does not require data to be passed, this object can be set to <see langword="null" />.</param>
 
     public void Execute(object parameter)
     {
---
+++ SunTaiLibrary/Helpers/UIElementHelper.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Windows;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SunTaiLibrary.Helpers
+{
+  /// <summary>
+  /// 提供元素布局相关的辅助功能，比如布局计算，大小计算。
+  /// </summary>
+  public static class UIElementHelper
+  {
+    /// <summary>
+    /// 根据一个容器大小，计算出指定比例可展示子内容的最大大小。
+    /// </summary>
+    /// <param name="boundSize">容器大小</param>
+    /// <param name="scale">子内容大小比例。【如：16 / 9d】</param>
+    /// <returns>计算后的子内容大小</returns>
+    public static Size ChildScaleSize(Size boundSize, double scale)
+    {
+      if (boundSize.IsEmpty || boundSize.Width == 0 || boundSize.Height == 0) return new Size(0, 0);
+
+      double bs = boundSize.Width / boundSize.Height;
+      if (bs == scale)
+      {
+        return boundSize;
+      }
+      else if (bs >= scale)
+      {
+        return new Size(boundSize.Height * scale, boundSize.Height);
+      }
+      else
+      {
+        return new Size(boundSize.Width, boundSize.Width / scale);
+      }
+    }
+  }
+}

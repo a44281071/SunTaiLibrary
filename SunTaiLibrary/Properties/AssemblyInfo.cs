6905b5b421e113e7cadf60c81887e6fe48787856
SamTi <a44281071@sina.com>
2020-08-11 18:23:40 +0800

    update to sdk_style project.

--- SunTaiLibrary/Properties/AssemblyInfo.cs
+++ SunTaiLibrary/Properties/AssemblyInfo.cs
@@ -1,21 +1,5 @@
-﻿using System.Reflection;
-using System.Resources;
-using System.Runtime.CompilerServices;
-using System.Runtime.InteropServices;
-using System.Windows;
+﻿using System.Windows;
 using System.Windows.Markup;
-
-// General Information about an assembly is controlled through the following
-// set of attributes. Change these attribute values to modify the information
-// associated with an assembly.
-[assembly: AssemblyTitle("SunTaiLibrary")]
-[assembly: AssemblyDescription("wpf library for myself")]
-[assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("Caritown")]
-[assembly: AssemblyProduct("SunTaiLibrary")]
-[assembly: AssemblyCopyright("Copyright ©  2018")]
-[assembly: AssemblyTrademark("Library for SamTi")]
-[assembly: AssemblyCulture("")]
 
 [assembly: XmlnsPrefix("http://www.suntai.org/library", "t3")]
 [assembly: XmlnsDefinition("http://www.suntai.org/library", "SunTaiLibrary.Controls")]
@@ -24,42 +8,11 @@ using System.Windows.Markup;
 [assembly: XmlnsDefinition("http://www.suntai.org/library", "SunTaiLibrary.Attached")]
 [assembly: XmlnsDefinition("http://www.suntai.org/library", "SunTaiLibrary.Commands")]
 
-// Setting ComVisible to false makes the types in this assembly not visible 
-// to COM components.  If you need to access a type in this assembly from 
-// COM, set the ComVisible attribute to true on that type.
-[assembly: ComVisible(false)]
-
-//In order to begin building localizable applications, set
-//<UICulture>CultureYouAreCodingWith</UICulture> in your .csproj file
-//inside a <PropertyGroup>.  For example, if you are using US english
-//in your source files, set the <UICulture> to en-US.  Then uncomment
-//the NeutralResourceLanguage attribute below.  Update the "en-US" in
-//the line below to match the UICulture setting in the project file.
-
-//[assembly: NeutralResourcesLanguage("en-US", UltimateResourceFallbackLocation.Satellite)]
-
-
-[assembly:ThemeInfo(
+[assembly: ThemeInfo(
     ResourceDictionaryLocation.None, //where theme specific resource dictionaries are located
-                             //(used if a resource is not found in the page,
-                             // or application resource dictionaries)
+                                     //(used if a resource is not found in the page,
+                                     // or application resource dictionaries)
     ResourceDictionaryLocation.SourceAssembly //where the generic resource dictionary is located
-                                      //(used if a resource is not found in the page,
-                                      // app, or any theme specific resource dictionaries)
+                                              //(used if a resource is not found in the page,
+                                              // app, or any theme specific resource dictionaries)
 )]
-
-
-// Version information for an assembly consists of the following four values:
-//
-//      Major Version
-//      Minor Version
-//      Build Number
-//      Revision
-//
-// You can specify all the values or you can default the Build and Revision Numbers
-// by using the '*' as shown below:
-// [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.2.2")]
-[assembly: AssemblyFileVersion("1.0.2.2")]
-[assembly: Guid("23110EFC-4EEA-4EFB-AD34-D9121C0D57B7")]
-

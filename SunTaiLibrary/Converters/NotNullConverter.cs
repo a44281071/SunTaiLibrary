0c0a299e1858a3cd40c52770f07755710fefbbd6
SamTi <a44281071@sina.com>
2022-03-04 15:36:11 +0800

    fix not null.

--- SunTaiLibrary/Converters/NotNullConverter.cs
+++ SunTaiLibrary/Converters/NotNullConverter.cs
@@ -12,7 +12,7 @@ namespace SunTaiLibrary.Converters
     {
         public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
         {
-            return value is null;
+            return value is not null;
         }
 
         public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)

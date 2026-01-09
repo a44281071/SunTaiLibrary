eff47433373feb579858030d174469e6bc088830
SamTi（三台） <a44281071@sina.com>
2026-01-09 15:10:56 +0800

    ver_1.1

--- SunTaiLibrary/Extensions/ExtensionIEnumerable.cs
+++ SunTaiLibrary/Extensions/ExtensionIEnumerable.cs
@@ -189,5 +189,150 @@ namespace System.Linq
         {
             return source is not null && source.Any();
         }
+
+        /// <summary>
+        /// 需要使用上一项值进行转换，使用类似于 Rx 的 Pairwise 操作符。
+        /// </summary>
+        /// <typeparam name="TSource"></typeparam>
+        /// <typeparam name="TResult"></typeparam>
+        /// <param name="source"></param>
+        /// <param name="selector">【TSource?：prev，TSource current】</param>
+        public static IEnumerable<TResult> Pairwise<TSource, TResult>(this IEnumerable<TSource> source, Func<TSource?, TSource, TResult> selector)
+        {
+            using var enumerator = source.GetEnumerator();
+
+            if (!enumerator.MoveNext())
+                yield break;
+
+            var previous = enumerator.Current;
+            var isFirst = true;
+
+            // 处理第一项（特殊情况）
+            if (isFirst)
+            {
+                yield return selector(default, previous);
+                isFirst = false;
+            }
+
+            while (enumerator.MoveNext())
+            {
+                var current = enumerator.Current;
+                yield return selector(previous, current);
+                previous = current;
+            }
+        }
+
+        /// <summary>
+        /// 根据相邻两项是否属于同一组的判断条件，将列表分段。
+        /// </summary>
+        /// <typeparam name="T">元素类型</typeparam>
+        /// <param name="source">源列表</param>
+        /// <param name="isSameGroup">判断当前项和前一项是否属于同一组的委托。参数：(previous, current)</param>
+        /// <returns>分段后的列表，每个分段是一个 List&lt;T&gt;</returns>
+        public static IEnumerable<List<T>> GroupByAdjacent<T>(
+            this IEnumerable<T> source,
+            Func<T, T, bool> isSameGroup)
+        {
+            if (source == null)
+                throw new ArgumentNullException(nameof(source));
+            if (isSameGroup == null)
+                throw new ArgumentNullException(nameof(isSameGroup));
+
+            using var enumerator = source.GetEnumerator();
+            if (!enumerator.MoveNext())
+                yield break; // 空序列
+
+            var currentGroup = new List<T> { enumerator.Current };
+
+            while (enumerator.MoveNext())
+            {
+                var previous = currentGroup[^1]; // 当前组最后一个元素即为前一项
+                var current = enumerator.Current;
+
+                if (isSameGroup(previous, current))
+                {
+                    currentGroup.Add(current);
+                }
+                else
+                {
+                    yield return currentGroup;
+                    currentGroup = [current];
+                }
+            }
+
+            if (currentGroup.Count > 0)
+                yield return currentGroup;
+        }
+
+        /// <summary>
+        /// 获取指定项在列表中左右相邻的元素（如果存在）。
+        /// 返回一个包含至多两个元素的列表：[左侧, 右侧]（如果存在）。
+        /// </summary>
+        /// <typeparam name="T">列表元素类型</typeparam>
+        /// <param name="list">源列表</param>
+        /// <param name="item">要查找的项</param>
+        /// <param name="comparer">可选的相等比较器，默认使用 EqualityComparer&lt;T&gt;.Default</param>
+        /// <returns>包含相邻项的列表（0~2 个元素）</returns>
+        public static List<T> GetAdjacentItems<T>(
+            this IList<T> list,
+            T item,
+            IEqualityComparer<T>? comparer = null)
+        {
+            if (list == null) return [];
+
+            comparer ??= EqualityComparer<T>.Default;
+
+            // 查找第一个匹配项的索引
+            int index = -1;
+            for (int i = 0; i < list.Count; i++)
+            {
+                if (comparer.Equals(list[i], item))
+                {
+                    index = i;
+                    break;
+                }
+            }
+
+            if (index == -1)
+                return []; // 未找到项
+
+            var result = new List<T>(2);
+
+            // 添加左侧（如果存在）
+            if (index > 0)
+                result.Add(list[index - 1]);
+
+            // 添加右侧（如果存在）
+            if (index < list.Count - 1)
+                result.Add(list[index + 1]);
+
+            return result;
+        }
+
+        /// <summary>
+        /// 根据指定索引，获取列表中左右相邻的元素（如果存在）。
+        /// 返回一个包含至多两个元素的列表：[左侧, 右侧]（如果存在）。
+        /// </summary>
+        /// <typeparam name="T">列表元素类型</typeparam>
+        /// <param name="list">源列表</param>
+        /// <param name="index">目标索引</param>
+        /// <returns>包含相邻项的列表（0~2 个元素）</returns>
+        public static List<T> GetAdjacentItemsByIndex<T>(this IList<T> list, int index)
+        {
+            if (list == null) return [];
+            if (index < 0 || index >= list.Count) return [];
+
+            var result = new List<T>(2);
+
+            // 左侧存在？
+            if (index > 0)
+                result.Add(list[index - 1]);
+
+            // 右侧存在？
+            if (index < list.Count - 1)
+                result.Add(list[index + 1]);
+
+            return result;
+        }
     }
 }
